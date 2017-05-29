173f7610292b343c71d10297b43ffb3a59285da5
pswiss <petrasswissler2022@u.northwestern.edu>
2017-05-29 16:28:09 -0500

    Created Android Project

---
+++ Pswiss_HW19/app/src/main/java/com/example/petrasswissler/pswiss_hw19/MainActivity.java
@@ -0,0 +1,191 @@
+package com.example.petrasswissler.pswiss_hw19;
+// libraries
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.SeekBar;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import static android.graphics.Color.blue;
+import static android.graphics.Color.green;
+import static android.graphics.Color.red;
+import static android.graphics.Color.rgb;
+import static android.hardware.Camera.Parameters.EFFECT_NEGATIVE;
+import static android.hardware.Camera.Parameters.FLASH_MODE_TORCH;
+
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
+    private Camera mCamera;
+    private TextureView mTextureView;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mSurfaceHolder;
+    private Bitmap bmp = Bitmap.createBitmap(640, 480, Bitmap.Config.ARGB_8888);
+    private Canvas canvas = new Canvas(bmp);
+    private Paint paint1 = new Paint();
+    private TextView mTextView;
+
+    SeekBar myControl;
+    TextView myTextView;
+
+    static long prevtime = 0; // for FPS calculation
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // keeps the screen from turning off
+
+        mTextView = (TextView) findViewById(R.id.cameraStatus);
+
+        myControl = (SeekBar) findViewById(R.id.seek1);
+
+        myTextView = (TextView) findViewById(R.id.textView01);
+        myTextView.setText("Threshold: 20");
+
+
+
+
+        setMyControlListener();
+
+        // see if the app has permission to use the camera
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            mSurfaceView = (SurfaceView) findViewById(R.id.surfaceview);
+            mSurfaceHolder = mSurfaceView.getHolder();
+
+            mTextureView = (TextureView) findViewById(R.id.textureview);
+            mTextureView.setSurfaceTextureListener(this);
+
+            // set the paintbrush for writing text on the image
+            paint1.setColor(0xffff0000); // red
+            paint1.setTextSize(24);
+
+            mTextView.setText("started camera");
+        } else {
+            mTextView.setText("no camera permissions");
+        }
+
+    }
+
+    private void setMyControlListener() {
+        myControl.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            int progressChanged = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+                myTextView.setText("Threshold: "+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        mCamera = Camera.open();
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPreviewSize(640, 480);
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_INFINITY); // no autofocusing
+        parameters.setAutoExposureLock(true); // keep the white balance constant
+        parameters.setFlashMode(FLASH_MODE_TORCH);
+        //parameters.setColorEffect(EFFECT_NEGATIVE);
+        mCamera.setParameters(parameters);
+        mCamera.setDisplayOrientation(90); // rotate to portrait mode
+
+        try {
+            mCamera.setPreviewTexture(surface);
+            mCamera.startPreview();
+        } catch (IOException ioe) {
+            // Something bad happened
+        }
+    }
+
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        // Ignored, Camera does all the work for us
+    }
+
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        mCamera.stopPreview();
+        mCamera.release();
+        return true;
+    }
+
+    // the important function
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        // every time there is a new Camera preview frame
+        mTextureView.getBitmap(bmp);
+
+        int targetLine =  bmp.getHeight() / 2;
+
+        double avgLineLocation = 0;
+        double numLinePixels;
+
+
+        final Canvas c = mSurfaceHolder.lockCanvas();
+        if (c != null) {
+
+            avgLineLocation = 0;
+            numLinePixels = 0;
+
+            int thresh = myControl.getProgress(); // comparison value
+            int[] pixels = new int[bmp.getWidth()]; // pixels[] is the RGBA data
+
+            bmp.getPixels(pixels, 0, bmp.getWidth(), 0, targetLine, bmp.getWidth(), 1);
+
+            for (int i = 0; i < bmp.getWidth(); i++) {
+                if ((green(pixels[i]) - red(pixels[i])) > thresh) {
+                    pixels[i] = rgb(0, 255, 0); // over write the pixel with pure green
+                    avgLineLocation += i;
+                    numLinePixels += 1;
+                }
+            }
+            // update the row
+            bmp.setPixels(pixels, 0, bmp.getWidth(), 0, targetLine, bmp.getWidth(), 1);
+
+            // Calc the average line Location
+            avgLineLocation = avgLineLocation / (numLinePixels +0.001);
+
+
+
+        }
+
+        // draw a circle at some position
+        int pos = (int) avgLineLocation;
+        canvas.drawCircle(pos, targetLine, 5, paint1); // x position, y position, diameter, color
+
+        // write the pos as text
+        canvas.drawText("pos = " + pos, 10, 200, paint1);
+        c.drawBitmap(bmp, 0, 0, null);
+        mSurfaceHolder.unlockCanvasAndPost(c);
+
+        // calculate the FPS to see how fast the code is running
+        long nowtime = System.currentTimeMillis();
+        long diff = nowtime - prevtime;
+        mTextView.setText("FPS " + 1000 / diff);
+        prevtime = nowtime;
+    }
+}
